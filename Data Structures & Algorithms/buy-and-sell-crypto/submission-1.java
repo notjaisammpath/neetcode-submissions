ce919066d5668e8f216b21660b2ea323b05720f6
Jai Aakash Sammpath <114313475+notjaisammpath@users.noreply.github.com>
2026-08-24 14:23:12 -0500

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int min = 100;
+        for(int i = 0; i < prices.length; i++) {
+            if(prices[i] < min) {
+                min = prices[i];
+                continue;
+            }
+            if(prices[i] - min > profit) {
+                profit = prices[i] - min;
+            }
+        }
+        return profit;
+    }
+}
